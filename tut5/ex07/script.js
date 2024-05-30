6342797f189b55f7de56c2b29008598bfaa76d91
your_name <ruth.kyx@gmail.com>
2024-05-31 01:02:51 +0800

    tut 5 pushed

--- tut5/ex07/script.js
+++ tut5/ex07/script.js
@@ -9,3 +9,27 @@ in the loop
 --> modify the className to cool
 */
 //console.log("hi");
+
+const hotItems = document.querySelectorAll('li.hot');
+
+// Loop through each hot item
+for (const item of hotItems) {
+  // Change class attribute to "cool"
+  item.classList.remove('hot');
+  item.classList.add('cool');
+}
+
+const hotElements = document.getElementsByClassName('hot');
+if(hotElements >= 3) {
+    hotElements[2].classList.remove('hot');
+    hotElements[2].classList.add('cool');
+}
+
+const listItems = getElementsByTagName('li');
+for(const item of listItems) {
+    if(item.classList.contains('hot')) {
+        item.classList.remove('hot');
+        item.classList.add('cool');
+        break; // break is needed so that the loop stops after first element is changed
+    }
+}
--- tut5/ex08/script.js
+++ tut5/ex08/script.js
@@ -1,3 +1,19 @@
-let text;
-let words;
-let characters;
+function initialize() {
+    let text = document.getElementsByClassName('text')[0];
+    let words = document.getElementsByClassName('words')[0];
+    let characters = document.getElementsByClassName('characters')[0];
+
+    let countChar = 0;
+    let countWord = 0;
+
+    text.addEventListener('input', function(){
+        const userInput = text.value;
+        // /\s/g is a search pattern that searches for whitespaces \s in the string; g is a gloabl search so all whitespaces are replaces instead of just one only
+        countChar = userInput.replace(/\s/g,'').length; 
+        countWord = userInput.trim().split(/\s+/).length;
+
+        words.textContent = countWord;
+        characters.textContent = countChar;
+    })
+}
+initialize();
